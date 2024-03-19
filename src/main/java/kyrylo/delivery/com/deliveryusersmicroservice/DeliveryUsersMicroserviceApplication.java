e412458f1ae0a4e2bd74e5712a8c77fba2f98ca3
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-19 19:51:11 +0100

    Initialize Delivery User Microservice

---
+++ src/main/java/kyrylo/delivery/com/deliveryusersmicroservice/DeliveryUsersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryusersmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryUsersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryUsersMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/DeliveryUsersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryusersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryUsersMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
