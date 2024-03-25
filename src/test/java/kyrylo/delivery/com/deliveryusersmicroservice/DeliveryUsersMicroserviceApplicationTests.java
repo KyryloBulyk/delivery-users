22e1e2bbc8ec4a4bfd0c20ef45b4dfd3a3600fe1
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-25 08:33:14 +0100

    Create Example Test

--- src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/DeliveryUsersMicroserviceApplicationTests.java
+++ src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/DeliveryUsersMicroserviceApplicationTests.java
@@ -1,10 +1,16 @@
 package kyrylo.delivery.com.deliveryusersmicroservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeliveryUsersMicroserviceApplicationTests {
 
+    @Test
+    @Disabled
+    void testForExample() {
+
+    }
 
 }
