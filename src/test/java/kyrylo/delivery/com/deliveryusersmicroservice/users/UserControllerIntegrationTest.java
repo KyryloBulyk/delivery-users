da4a96aaab119705e5d7affd250a5caa7da4a958
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-28 01:32:13 +0100

    Created 2 test for 2 endpoints in UserController

--- src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/users/UserControllerIntegrationTest.java
+++ src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/users/UserControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import kyrylo.delivery.com.deliveryusersmicroservice.entities.User;
 import kyrylo.delivery.com.deliveryusersmicroservice.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,8 @@ public class UserControllerIntegrationTest {
     private static boolean alreadySetup = false;
     private String jwtToken;
     private User sampleUser;
+
+    private static Long userId;
     private Logger logger = LoggerFactory.getLogger(UserControllerIntegrationTest.class);
 
     @BeforeAll
@@ -40,17 +43,21 @@ public class UserControllerIntegrationTest {
         if (!alreadySetup) {
             RegisterRequest registerRequest = new RegisterRequest("username", "password", "useremail@example.com", "ROLE_ADMIN");
 
-            mockMvc.perform(post("/api/auth/register")
+            MvcResult registerResult = mockMvc.perform(post("/api/auth/register")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(registerRequest)))
-                    .andExpect(status().isCreated());
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            User registeredUser = objectMapper.readValue(registerResult.getResponse().getContentAsString(), User.class);
+            userId = registeredUser.getUserId();
 
             alreadySetup = true;
         }
     }
 
     @BeforeEach
-    void loginBeforeEachTest() throws Exception {
+    void loginBeforeEachTest(@Autowired MockMvc mockMvc, @Autowired ObjectMapper objectMapper, @Autowired UserRepository userRepository) throws Exception {
         AuthRequest authRequest = new AuthRequest("username", "password");
         MvcResult result = mockMvc.perform(post("/api/auth/token")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -63,21 +70,30 @@ public class UserControllerIntegrationTest {
         this.jwtToken = jwtResponse.accessToken();
 
         sampleUser = new User();
+        sampleUser.setUserId(userId);
+        sampleUser.setUsername("username");
+        sampleUser.setEmail("useremail@example.com");
+        sampleUser.setPassword("password");
     }
 
-//    @Test
-//    public void getAllUsersTest() throws Exception {
-//        User sampleUser = new User();
-//        sampleUser.setUserId(1L); // Припустимо, ID користувача 1
-//        // Тут ви можете встановити інші поля sampleUser
-//
-//        when(userService.getAllUsers()).thenReturn(Arrays.asList(sampleUser)); // Налаштування моку
-//
-//        mockMvc.perform(get("/api/users")
-//                        .header("Authorization", "Bearer " + jwtToken)) // Використання токена
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$[0].id").value(sampleUser.getUserId()));
-//    }
+    @Test
+    public void getAllUsers_ShouldReturnOk() throws Exception {
+        mockMvc.perform(get("/api/users")
+                        .header("Authorization", "Bearer " + this.jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].username").value("username"));
+    }
+
+    @Test
+    public void getUserById_ShouldReturnUser() throws Exception {
+        mockMvc.perform(get("/api/users/{userId}", sampleUser.getUserId())
+                        .header("Authorization", "Bearer " + this.jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value(sampleUser.getUsername()));
+    }
+
 }
 
