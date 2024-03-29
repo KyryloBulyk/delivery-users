63c25ff88d4fb271f8872e2efe908ec620e12ae0
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-29 01:18:47 +0100

    Change username for register user

--- src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/roles/RoleControllerIntegrationTest.java
+++ src/test/java/kyrylo/delivery/com/deliveryusersmicroservice/roles/RoleControllerIntegrationTest.java
@@ -1,12 +1,10 @@
 package kyrylo.delivery.com.deliveryusersmicroservice.roles;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kyrylo.delivery.com.deliveryusersmicroservice.dto.AuthRequest;
 import kyrylo.delivery.com.deliveryusersmicroservice.dto.JwtResponse;
 import kyrylo.delivery.com.deliveryusersmicroservice.dto.RegisterRequest;
 import kyrylo.delivery.com.deliveryusersmicroservice.entities.Role;
-import kyrylo.delivery.com.deliveryusersmicroservice.entities.User;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -36,7 +33,7 @@ public class RoleControllerIntegrationTest {
 
     @BeforeAll
     public static void setUpOnce(@Autowired MockMvc mockMvc, @Autowired ObjectMapper objectMapper) throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest("username", "password123", "useremail@example.com", "ROLE_ADMIN");
+        RegisterRequest registerRequest = new RegisterRequest("username2", "password123", "useremail@example.com", "ROLE_ADMIN");
 
         mockMvc.perform(post("/api/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -47,7 +44,7 @@ public class RoleControllerIntegrationTest {
 
     @BeforeEach
     void loginBeforeEachTest(@Autowired MockMvc mockMvc, @Autowired ObjectMapper objectMapper) throws Exception  {
-        AuthRequest authRequest = new AuthRequest("username", "password123");
+        AuthRequest authRequest = new AuthRequest("username2", "password123");
         MvcResult result = mockMvc.perform(post("/api/auth/token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(authRequest)))
